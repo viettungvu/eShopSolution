8bf9f992d73a21518f7e0424780f134710f0acb4
= <=>
2021-05-07 16:09:01 +0700

    Biuld Application Layer

---
+++ eShopSolution.Ultilities/Exceptions/EShopException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Ultilities.Exceptions
+{
+    public class EShopException:Exception
+    {
+        public EShopException()
+        {
+
+        }
+        public EShopException(string message):base(message)
+        {
+
+        }
+        public EShopException(string message, Exception inner) : base(message, inner)    
+        {
+            
+        }
+    }
+}
